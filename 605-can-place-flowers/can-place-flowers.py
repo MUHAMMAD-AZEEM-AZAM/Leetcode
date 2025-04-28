e1050976f0f6d7e1c18ed2a73c3e1cbcfd187c4d
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-04-29 01:02:15 +0500

    Time: 11 ms (37.81%) | Memory: 18.3 MB (14.46%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -8,10 +8,9 @@ class Solution:
             if flowerbed[i]==1:
                 flag=0
             if flag==3 or (i==1 and flowerbed[0]==0 and flowerbed[1]==0) or (i==len(flowerbed)-1 and flowerbed[i]==0 and flowerbed[i-1]==0):
-                
                 totalCount+=1
-                flag=1   
-            print(i)    
-        print(totalCount)         
-        return totalCount>=n        
+                flag=1  
+            if totalCount>=n:
+                return True     
+        return False       
         
