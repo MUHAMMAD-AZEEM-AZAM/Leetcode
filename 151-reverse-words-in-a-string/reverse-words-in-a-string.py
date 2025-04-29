c30fd13f3d9852b90780de26178979ba01eb8464
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-04-29 22:36:41 +0500

    Time: 6 ms (10.05%) | Memory: 17.8 MB (91.78%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,5 +1,17 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-     return ' '.join(reversed(s.split()))     
+        s=s[::-1]
+        j=0
+        st=[]
+        for i in range(len(s)):
+            if s[i]==" ":
+               sub=s[j:i]
+               if sub!="":
+                  st.append(sub[::-1])
+               j=i+1
+        sub=s[j:len(s)]
+        if sub!="":
+            st.append(sub[::-1])       
+        return ' '.join(st)
 
         
