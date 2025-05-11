16c3470350a323e7f37a89e20580536d579c0100
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-05-11 17:01:40 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.46%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -3,9 +3,6 @@ class Solution:
         maximum=max(candies)
         result=[]
         for index in range(len(candies)):
-            if candies[index]+extraCandies<maximum:
-                result.append(False)
-            else:
-                result.append(True)
+            result.append(not candies[index]+extraCandies<maximum)
         return result            
 
