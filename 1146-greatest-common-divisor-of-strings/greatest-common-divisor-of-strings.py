9a7508ee42a6cc4e6e2b8905590e538ef62c9c52
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-05-11 16:45:16 +0500

    Time: 19 ms (5.13%) | Memory: 17.9 MB (25.54%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,14 +1,36 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        len1, len2 = len(str1), len(str2)
-        def valid(k):
-            if len1%k or len2%k:
+        def divides(st,subS):
+            print("Called")
+            if len(st)%len(subS)!=0:
+                print("not divisible")
                 return False
-            n1,n2=len1//i,len2//i
-            base=str1[:i]
-            return str1==n1*base and str2==n2*base   
+            n=0    
+            for i in range(int(len(st)/len(subS))):
+                if st[n:n+len(subS)]!=subS:
+                    print("not equal",st[i:len(subS)])
+                    return False
+                print("True")    
+                n=n+len(subS)    
+            return True          
 
-        for i in range(min(len1,len2),0,-1):
-            if valid(i):
-                return str1[:i]
-        return ""        
+        check=0
+        minimum=min(len(str1),len(str2))
+        for i in range(minimum):
+            if str1[i]!=str2[i]:
+                break
+            check+=1
+        subString=str1[0:check]
+        print(subString)
+
+
+        for i in range(check):
+            if divides(str1,subString) and divides(str2,subString):
+                return subString 
+            check-=1      
+            subString=str1[0:check]
+            print(subString)
+
+        return ""          
+
+
