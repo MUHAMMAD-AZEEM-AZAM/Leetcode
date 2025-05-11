a469a412f9e4cda6986db214133b6e8fd2ca85ca
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-05-12 00:21:48 +0500

    Time: 7 ms (91.23%) | Memory: 18.5 MB (92.06%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def reverseVowels(self, s):
-        vowels="aeiouAEIOU"
-        s=list(s)
+        vowels='aeiouAEIOU'     
         i,j=0,len(s)-1
-
+        s=list(s)
         while i<j:
             while i<j and s[i] not in vowels:
                 i+=1
@@ -12,4 +11,6 @@ class Solution(object):
             s[i],s[j]=s[j],s[i]
             i+=1
             j-=1
-        return "".join(s)            
+        return "".join(s)   
+
+
