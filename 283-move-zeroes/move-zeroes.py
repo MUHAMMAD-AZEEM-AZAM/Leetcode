52bf9b4d8f542094da93f5fff9ea1926160666a0
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-05-12 13:29:26 +0500

    Time: 6 ms (49.38%) | Memory: 18.7 MB (79.07%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,25 +1,30 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         j=0
-        i=0
-        e=len(nums)-1
-        flag=False
-        while i<=e:
+        for i in range(len(nums)):
             if nums[i]!=0:
-                i+=1
-                continue
-            j=i    
-            while j<e:
-                nums[j]=nums[j+1]
-                if nums[j]==0:
-                    flag=True
+                nums[i],nums[j]=nums[j],nums[i]
                 j+=1
-            nums[e]=0
-            e-=1 
-            if flag:
-                i=0
-            else:
-                i+=1   
+
+        # i=0
+        # e=len(nums)-1
+        # flag=False
+        # while i<=e:
+        #     if nums[i]!=0:
+        #         i+=1
+        #         continue
+        #     j=i    
+        #     while j<e:
+        #         nums[j]=nums[j+1]
+        #         if nums[j]==0:
+        #             flag=True
+        #         j+=1
+        #     nums[e]=0
+        #     e-=1 
+        #     if flag:
+        #         i=0
+        #     else:
+        #         i+=1   
 
 
 
