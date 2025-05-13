77590e098334336edddef25755151ab1a5450eaf
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-05-13 14:11:09 +0500

    Time: 71 ms (88.56%) | Memory: 28.6 MB (24.21%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,13 +3,11 @@ class Solution:
         start=0
         end=len(height)-1
         area=0
-        def cal_area():
+   
+        for i in range(len(height)):
             h=min(height[start],height[end])
             w=end-start
-            return h*w
-        area=cal_area()    
-        for i in range(len(height)):
-            new_area=cal_area()
+            new_area= h*w
             if area<new_area:
                 area=new_area
             if height[start]<height[end] and start<end:
