1a52f3abcb0095e2672badca0840f3ed5209d2a5
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-06-03 00:22:15 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.66%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -2,15 +2,12 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         freq_map = {}
 
-        # Count frequencies
-        for num in arr:
-            freq_map[num] = freq_map.get(num, 0) + 1
+        for nums in arr:
+            freq_map[nums]=freq_map.get(nums,0)+1
+        freq_set  =set()
 
-        # Check if all frequencies are unique
-        freq_set = set()
         for freq in freq_map.values():
             if freq in freq_set:
                 return False
-            freq_set.add(freq)
-
-        return True
+            freq_set.add(freq)   
+        return True     
