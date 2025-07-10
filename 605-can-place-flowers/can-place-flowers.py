f1425d3927ca3d2d0e378b73a7798789ee8b83e7
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-07-10 21:50:20 +0500

    Time: 13 ms (20.53%) | Memory: 18.1 MB (54.37%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,10 +1,13 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        
-        plants = sum(flowerbed)
-        flowerbed = [0] + flowerbed + [0]
-        for i in range(1, len(flowerbed)-1):
-            if flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0:
-                flowerbed[i] = 1
+              for i in range(len(flowerbed)):
+                left = i == 0 or flowerbed[i-1] == 0
+                right = i == len(flowerbed)-1 or flowerbed[i+1] == 0
 
-        return sum(flowerbed) - plants >= n
+                if left and right and flowerbed[i] == 0:
+                    n-=1
+                    flowerbed[i] = 1
+
+              return n <= 0
+
+        
