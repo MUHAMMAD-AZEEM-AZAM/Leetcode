b4805712efd138f87ce800abb098f92c8b68bf0d
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-08-05 18:08:18 +0500

    Time: 162 ms (13.53%) | Memory: 21.9 MB (83.96%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -1,18 +1,15 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        row_map = {}
-
-        # Count how many times each row appears (as a tuple)
-        for row in grid:
-            key = tuple(row)
-            row_map[key] = row_map.get(key, 0) + 1
-
-        count = 0
-        # For each column, check if its tuple form exists in row_map
-        for j in range(n):
-            col = tuple(grid[i][j] for i in range(n))
-            if col in row_map:
-                count += row_map[col]
-
-        return count
+        length = len(grid[0])
+        column = [[0 for _ in range(length)] for _ in range(length)]
+        count =0
+        for i in range(length):
+            for j in range(length):
+                column[i][j] =grid[j][i]
+            
+        for col in column:
+            for row in grid:
+                if col == row:
+                    count+=1
+        return count        
+            
