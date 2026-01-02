cc676e9d56022a5e65340afa76d2ca8fed020038
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2026-01-02 10:37:20 -0500

    Time: 55 ms (5.47%) | Memory: 18.9 MB (23.01%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        n = len(nums)/2
+        my_map = {}
+        for i in nums:
+            my_map[f'{i}'] = 0
+        for i in nums:
+            my_map[f'{i}'] =my_map[f'{i}'] + 1 
+            if my_map[f'{i}'] == n:
+                print(my_map)
+                return int(i) 
