281f955f1979414c926916a9bc4ef7d492555fac
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2026-01-02 10:41:05 -0500

    Time: 8 ms (42.48%) | Memory: 18.8 MB (41.46%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,10 +1,15 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        n = len(nums)/2
-        my_map = {}
-        for i in nums:
-            my_map[f'{i}'] = 0
-        for i in nums:
-            my_map[f'{i}'] =my_map[f'{i}'] + 1 
-            if my_map[f'{i}'] == n:
-                return int(i) 
+        # n = len(nums)/2
+        # my_map = {}
+        # for i in nums:
+        #     my_map[f'{i}'] = 0
+        # for i in nums:
+        #     my_map[f'{i}'] =my_map[f'{i}'] + 1 
+        #     if my_map[f'{i}'] == n:
+        #         return int(i) 
+
+        count = collections.Counter(nums)
+        for k in count:
+            if count[k] >1:
+                return k
