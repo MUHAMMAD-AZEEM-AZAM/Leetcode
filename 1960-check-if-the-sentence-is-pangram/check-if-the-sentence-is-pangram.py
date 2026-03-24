fec020149cf213d55489c23fb11bd3b42576398f
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2026-03-24 04:40:54 -0400

    Time: 2 ms (20.17%) | Memory: 19.4 MB (39.27%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,5 +1,7 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
+        if len(sentence)<26:
+            return False
         counter = collections.Counter(sentence)   
         if len(counter) < 26:
             return False
