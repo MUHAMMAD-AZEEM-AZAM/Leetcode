96d3c06091874dd4d1affb0b019e3ba5838e887b
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2026-04-08 03:52:11 -0400

    Time: 67 ms (8.31%) | Memory: 25.7 MB (11.77%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -3,11 +3,28 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.nums = nums
+        self.nums.sort()
 
     def add(self, val: int) -> int:
-        self.nums.append(val)
-        self.nums.sort()
+        index = self.getIndex(val)
+        self.nums.insert(index, val)
         return self.nums[-self.k]
+
+    def getIndex(self, val: int) -> int:
+        #Binary search
+        left, right = 0, len(self.nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            mid_number = self.nums[mid]
+
+            if val > mid_number:
+                left = mid + 1
+            elif val < mid_number:
+                right = mid-1
+            else:
+                return mid
+        return left
+        
 
         
 
